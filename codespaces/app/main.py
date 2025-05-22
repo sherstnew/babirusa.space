c4766d455f98765c2cd23bb6d789aafd9d283b36
rediska <sherstnev.denis.v@gmail.com>
2025-05-22 20:16:42 +0300

    fix

--- codespaces/app/main.py
+++ codespaces/app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from app import MONGO_DSN, ENVIRONMENT, projectConfig
-from app.routers import codespaces, resolver, 
+from app.routers import codespaces
 
 if ENVIRONMENT == "prod":
     app = FastAPI(
@@ -21,7 +21,6 @@ else:
     )
 
 app.include_router(codespaces.router)
-app.include_router(resolver.router)
 
 @app.on_event('startup')
 async def startup_event():
