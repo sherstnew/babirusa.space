dd3cb27dfa2e57e857e05691db12582804c75cdc
rediska <sherstnev.denis.v@gmail.com>
2025-05-31 14:09:13 +0300

    mongo init

---
+++ mongo-init.js
@@ -0,0 +1,20 @@
+const babirusaUser = process.env.BABIRUSA_USER;
+const babirusaPassword = process.env.BABIRUSA_PASSWORD;
+
+db = db.getSiblingDB('babirusa');
+db.createCollection('init');
+db.createUser({
+  user: babirusaUser,
+  pwd: babirusaPassword,
+  roles: [{ role: 'readWrite', db: 'babirusa' }]
+});
+
+db = db.getSiblingDB('babirusa-test');
+db.createCollection('init');
+db.createUser({
+  user: babirusaUser,
+  pwd: babirusaPassword,
+  roles: [{ role: 'readWrite', db: 'babirusa-test' }]
+});
+
+print('Database initialization completed!');
